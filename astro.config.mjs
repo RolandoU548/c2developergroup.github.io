4d118699178373fa926352bfc24b01707c29e5fc
Rolando <rolandou548@gmail.com>
2025-03-26 02:52:13 -0400

    workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: 'https://RolandoU548.github.io',
+  base:'c2developergroup.github.io',
   integrations: [
     tailwind(),
     react({
